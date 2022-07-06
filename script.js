ff87e157a57d171b9426259c99bcbd6ab8ec8e51
ANANYA KANNAN <104369167+velvet-facade@users.noreply.github.com>
2022-07-07 02:20:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+const pwEl = document.getElementById("pw");
+const lenEl = document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEL = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+const copyEl = document.getElementById("copy");
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+";
+
+function getLowercase() {
+  return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+}
+function getUppercase() {
+  return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+function getNumber() {
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+function getSymbol() {
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generatePw() {
+  const len = lenEl.value;
+  let password = "";
+
+  if (upperEl.checked) {
+    password += getUppercase();
+  }
+  if (lowerEL.checked) {
+    password += getLowercase();
+  }
+
+  if (numberEl.checked) {
+    password += getNumber();
+  }
+  if (symbolEl.checked) {
+    password += getSymbol();
+  }
+  for (let i = password.length; i < len; i++) {
+    const x = generateX();
+    password += x;
+  }
+  pwEl.innerText = password;
+}
+
+function generateX() {
+  const xs = [];
+  if (upperEl.checked) {
+    xs.push(getUppercase());
+  }
+  if (lowerEL.checked) {
+    xs.push(getLowercase());
+  }
+  if (numberEl.checked) {
+    xs.push(getNumber());
+  }
+  if (symbolEl.checked) {
+    xs.push(getSymbol());
+  }
+  if (xs.length === 0) {
+    return "";
+  }
+  return xs[Math.floor(Math.random() * xs.length)];
+}
+
+generateEl.addEventListener("click", generatePw);
+
+copyEl.addEventListener("click", () => {
+  password.select();
+  navigator.clipboard.writeText(password.value);
+  alert("Password copied to clipboard" + password);
+});
